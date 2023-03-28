9635afb08ac88f2e078b47c533551e0e4a769d48
Roger Marques <marquesroger226@gmail.com>
2023-03-28 17:38:06 -0300

    [feat]: model and migration Teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: String(30),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
