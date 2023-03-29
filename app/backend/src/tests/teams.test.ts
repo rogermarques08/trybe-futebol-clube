037ac5fd2f106b417f73c1ce74d799faa245c5da
Roger Marques <marquesroger226@gmail.com>
2023-03-28 22:06:03 -0300

    [feat]: add GET /teams tests

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,33 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import Teams from '../database/models/team.model';
+
+import { Response } from 'superagent';
+import { app } from '../app';
+import teamsMock from './mocks/teams.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon.stub(Teams, 'findAll').resolves(teamsMock as Teams[]);
+  });
+
+  afterEach(() => {
+    (Teams.findAll as sinon.SinonStub).restore();
+  });
+
+  it('Testa se é possível solicitar todos os times (GET /teams)', async () => {
+    chaiHttpResponse = await chai.request(app).get('/teams');
+
+    expect(chaiHttpResponse.body).to.be.deep.equal(teamsMock);
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
+});
