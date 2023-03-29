15b082c125f80a892150bf3ca8b6914a83ab3631
Roger Marques <marquesroger226@gmail.com>
2023-03-29 14:48:04 -0300

    [feat]: add model and migration users

---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,45 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class UserModel extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  },
+);
+
+export default UserModel;
