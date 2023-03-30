9a8e546be1f157961466ab31668cb1da3ce35c02
Roger Marques <marquesroger226@gmail.com>
2023-03-30 16:02:20 -0300

    [feat]: add GET /matches?inProgress

--- app/backend/src/database/controllers/match.controller.ts
+++ app/backend/src/database/controllers/match.controller.ts
@@ -2,7 +2,18 @@ import { Request, Response } from 'express';
 import matchService from '../services/match.service';
 import mapTypes from '../utils/mapTypes';
 
+const getInProgress = async (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+  const query = inProgress === 'true';
+
+  const { type, message } = await matchService.getInProgress(query);
+
+  return res.status(mapTypes(type)).json(message);
+};
+
 const getAllMathces = async (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+  if (inProgress !== undefined) return getInProgress(req, res);
   const { type, message } = await matchService.getAllMathces();
 
   return res.status(mapTypes(type)).json(message);
@@ -10,6 +21,7 @@ const getAllMathces = async (req: Request, res: Response) => {
 
 const matchController = {
   getAllMathces,
+  getInProgress,
 };
 
 export default matchController;
--- app/backend/src/database/services/match.service.ts
+++ app/backend/src/database/services/match.service.ts
@@ -13,8 +13,21 @@ const getAllMathces = async (): Promise<ITypeMessage> => {
   return { type: 'success', message: matches };
 };
 
+const getInProgress = async (inProgress: boolean): Promise<ITypeMessage> => {
+  const matches = await MatchModel.findAll({
+    include: [
+      { model: TeamModel, as: 'homeTeam', attributes: { exclude: ['id'] } },
+      { model: TeamModel, as: 'awayTeam', attributes: { exclude: ['id'] } },
+    ],
+    where: { inProgress },
+  });
+
+  return { type: 'success', message: matches };
+};
+
 const matchService = {
   getAllMathces,
+  getInProgress,
 };
 
 export default matchService;
