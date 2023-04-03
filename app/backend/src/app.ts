926572d760d01243b71f9199ce6075c6bccf722e
Roger Marques <marquesroger226@gmail.com>
2023-04-03 20:25:25 -0300

    [feat]: .

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -12,7 +12,7 @@ class App {
 
     this.config();
 
-    // Não remover essa rota
+    // Não remover essa rota.
     this.app.get('/', (req, res) => res.json({ ok: true }));
 
     this.app.use('/teams', TeamRoutes);
